572f2462887577040470537a126ba773904dea1e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-04 20:01:27 +0200

    Food buying app

--- src/main/scala/MyApp.scala
+++ src/main/scala/MyApp.scala
@@ -1,11 +1,24 @@
+import scala.io.StdIn.readLine //so intelliJ will suggest what library needs importing
+
 object MyApp extends App{
   println("My first Scala program!")
-  print("So print has no new line")
-  print("As can be seen here")
-  println() //just an empty line //is a comment meaning for humans not for computer
-  //comments are ignored by computer
-  //comments are best when they explain why you did something
-  println("Hmm more code!") //saying print is printing something is not very helpful :)
+//  print("So print has no new line")
+//  print("As can be seen here")
+//  println() //just an empty line //is a comment meaning for humans not for computer
+//  //comments are ignored by computer
+//  //comments are best when they explain why you did something
+//  println("Hmm more code!") //saying print is printing something is not very helpful :)
   //we just adding new instruction but we will want to save some data
   //most likely we will want to branch depending on what that data is
+  val myName = readLine("What is your name friend? ") //readLine will return Strings
+  val favoriteFood = readLine(s"Hello there $myName what is your favorite food?") //s string interpolation lets us insert values and variables directly into string
+  println(s"Cool I like $favoriteFood as well!")
+  val currentPrice = readLine(s"How much is/are $favoriteFood per kg in stores right now?").toFloat //getting is/are correct will need if statements
+  println(s"Not bad I could buy some $favoriteFood for $currentPrice.")
+  val currentMoney = readLine(s"How much money do you have in you wallet $myName?").toFloat
+  //TODO find out how much of our favorite food we could afford right now
+  val maxPurchaseWeight = currentMoney / currentPrice  //so Scala (in contrast to Java where you have to spell it out) picks up/infers the data type
+//  println(s"Congratulations $myName! You can buy ${(maxPurchaseWeight*100).round/100.toDouble} kilos of $favoriteFood right now.")
+  println(s"Congratulations $myName! You can buy ${(maxPurchaseWeight*100).round/100.0} kilos of $favoriteFood right now.")
+
 }
--- src/main/scala/hello.sc
+++ src/main/scala/hello.sc
@@ -24,6 +24,20 @@ val bigResult = myVar + math.pow(2,16) //if our variable has 2 or more names we 
 //double is really a double sized floating point number
 println(result.getClass, bigResult.getClass)
 //so we have string
-val myName = "Valdis"
+val myName = "Valdis" // " for strings
 println(myName, myName.getClass)
-
+val singleChar = 'v' //so ' for chars
+println(singleChar, singleChar.getClass)
+val myByte : Byte = 120
+println(myByte, myByte.getClass)
+val anotherByte : Byte = 10
+val resultByte = (myByte + anotherByte).toByte
+//so you can cast one data type to another (not all conversions will work)
+val myNumberString = "125628"
+val myNumber = myNumberString.toInt
+println(myNumberString, myNumber) //printing does not show that those are two differrent data types
+println(myNumberString.getClass, myNumber.getClass) //printing does not show that those are two differrent data types
+//boolean data types, these we will use when we need some logic in our program
+val isRaining = false
+val isSnowing = true
+val isCold = true
