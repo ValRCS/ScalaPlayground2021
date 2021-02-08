a0a9c62321d19d730399ab765bde8e7806d6c97f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-08 21:33:50 +0200

    if else exercises solutions

--- src/main/scala/BonusCalculator.scala
+++ src/main/scala/BonusCalculator.scala
@@ -1,9 +1,9 @@
 import scala.io.StdIn.readLine
-
+//from https://github.com/LauSla/Scala-stuff/blob/main/BonusCalculator.scala
 object BonusCalculator extends App {
   //TODO Calculate yearly Xmas Bonus
   //Ask for Employee Name
-  val employee = readLine("What is your name?")
+  val employee = readLine("What is your name? ")
   println(s"Nice to talk to you $employee!")
 
   //Ask for how long they have worked
@@ -11,4 +11,24 @@ object BonusCalculator extends App {
   //Calculate Xmas bonus if they have worked at least 2  years
   //Bonus is years worked over 2 years * 15% of monthly wage
   //so 5 years worked, 1000 Euros wage would give 450 Euro bonus
+  //Ask for how long they have worked
+  val tenure = readLine("How many years have you worked here for? ").toFloat //convert to number
+  //ask for monthly wage
+  val salary = readLine(s"And how much do we pay you, $employee? ").toFloat //convert to number
+  //Calculate Xmas bonus if they have worked at least 2  years
+  //Bonus is years worked over 2 years * 15% of monthly wage
+  //so 5 years worked, 1000 Euros wage would give 450 Euro bonus
+  //technically this would not be needed for tenure under 2
+  //this could be important if the calculation was over the network, required some slow database
+  val minYears = 2 //potentially this value could come from some external source/file, database, network,
+
+  if (tenure < minYears) {
+    println("Sorry, no bonus for you!")
+  }
+  else {
+    val bonus = salary * (tenure.ceil - minYears) * 0.15 //.floor rounds it down, count FULL years only
+    println(s"$employee, your bonus is $bonus!")
+  }
+  //there is no bonus here //it only lives in the else block
+//  println(bonus)
 }
--- src/main/scala/TemperatureTester.scala
+++ src/main/scala/TemperatureTester.scala
@@ -1,7 +1,23 @@
+import scala.io.StdIn.readLine
+// from https://github.com/LauSla/Scala-stuff/blob/main/TemperatureTester.scala
+
 object TemperatureTester extends App {
   //TODO write a program which asks for user's temperature
   //if Temperature is over 37C print a warming about high temperature
   //if temperature is under 35 print a warning about being cold
   //otherwise print that everything is great :0
+  val feverTemp = 37
+  val coldTemp = 35
+  val patient = readLine("What is your name?")
+  val myTemp = readLine(s"What is your temperature $patient?").toFloat //convert to number
+  if (myTemp >= feverTemp) {
+    println("You've got a fever!")
+  }
+  else if (myTemp <= coldTemp) {
+    println("You've got hypothermia!")
+  }
+  else {
+    println("You're doing great!")
+  }
 
 }
