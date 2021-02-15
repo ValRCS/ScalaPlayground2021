b733ed4b04797fdf583803a7adc847f8e0413e0c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-15 20:07:44 +0200

    Random and Cube exercises

---
+++ src/main/scala/CubeTable.scala
@@ -0,0 +1,9 @@
+import scala.io.StdIn.readLine
+
+object CubeTable extends App {
+  //TODO write a program that takes user input for starting and end values
+  //then saves cubes of those values in a Seq data type and then prints the cubes out
+  val begVal = readLine("What is the starting value?")
+  println(s"Will print cubes starting with cube for $begVal")
+
+}
