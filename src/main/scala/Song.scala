290f48991fc0f24844e487fcbcfdf997f814f728
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-22 21:14:47 +0200

    Song exercise solved

--- src/main/scala/Song.scala
+++ src/main/scala/Song.scala
@@ -5,6 +5,29 @@ class Song(title:String, author:String, lyrics: Seq[String]) {
   //Sequence of lyrics
   //when we create a new object from Song class print the title author and that song is made
 
+  //when we create a new object from Song class print the title author and that song is made
+  println(s"$author: $title <-- New Song created!")
+
+  //TODO methods to sing song line by line on the screen
+  def singSong() = {
+    lyrics.foreach(println)
+  }
+  //TODO method yell (ALL CAPS) the lyrics on the screen
+  def yellSong() = {
+    for (line <- lyrics)
+      println(line.toUpperCase())
+  }
+
+  def singBonus(times:Int = -1): Unit = {
+    if (times == -1) lyrics.foreach(println)
+    else lyrics.slice(0,times).foreach(println)
+//    else {
+//      for (line <- lyrics.slice(0, times))
+//        println(line)
+//    }
+
+  }
+
 
   //TODO methods to sing song line by line on the screen
 
