2612fdf72d8ea2009e298b59f3144d2f28ee8a80
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-23 19:26:14 +0200

    More Traits usage

--- src/main/scala/Frog.scala
+++ src/main/scala/Frog.scala
@@ -1,3 +1,16 @@
-class Frog extends Philosophical {
+class Animal(val hasVertebrae: Boolean = true) {
+  def scratchBack = s"scratches"
+}
+
+//so in scala we can extend a single class or trait
+//then we can add additional traits only with with ... :)
+class Frog extends Animal with Philosophical with HasLegs  {
   override def toString = "greenie" //rewriting default functionality of toString
 }
+
+trait HasLegs {
+  var legCount = 4
+  //naming of howFar is not perfect because we do not know the units
+  def jump(howFar: Double) = s"Print jump on all $legCount legs $howFar meters far"
+}
+
--- src/main/scala/UsingTraits.scala
+++ src/main/scala/UsingTraits.scala
@@ -1,5 +1,11 @@
 object UsingTraits extends App {
   val kermit = new Frog
   kermit.philosophize
+  println(kermit.jump(1.2))
   println(kermit)
+  val axel = new Frog
+  axel.philosophize
+  println(axel.jump(30))
+  axel.legCount = 2 //we can change it since it is a variable
+  println(kermit.legCount, axel.legCount)
 }
