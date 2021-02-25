a512d27120c6b1982c7b39fd4fb6e70332608ead
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-02-25 21:30:17 +0200

    Two Roads and reading files

---
+++ src/main/scala/ReadingFiles.scala
@@ -0,0 +1,24 @@
+import scala.io.Source
+
+object ReadingFiles extends App {
+  println("Reading files")
+  val absolute_path = "c:/Temp/sample-text-file.txt" //choose your own text file here
+//  for (line <- Source.fromFile(absolute_path).getLines) {
+//    println(line)
+//  }
+//  val lines = Source.fromFile(absolute_path).getLines.toArray
+//  lines.foreach(println)
+  //better get a reference to our file buffer(stream) and then close it properly when done
+  val bufferedSource = Source.fromFile(absolute_path)
+  val lines = bufferedSource.getLines.toArray
+  bufferedSource.close //now file is properly closed (assuming no exceptions)
+
+  //now we can work with file contents
+  lines.foreach(println)
+  //we can split each line into individual words, in this case by whitespace
+  val lines_tokenized = lines.map(line => line.split(" "))
+  lines_tokenized.foreach(line => {line.foreach(println); println("*"*20)})
+
+
+
+}
