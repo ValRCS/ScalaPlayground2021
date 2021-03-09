9b6c3a6dc0a50fc4f3d773795b3fc13d93369aa7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-03-09 20:10:40 +0200

    XML parsing exercise

--- build.sbt
+++ build.sbt
@@ -3,3 +3,7 @@ name := "ScalaPlayground2021"
 version := "0.1"
 
 scalaVersion := "2.13.4"
+
+// https://mvnrepository.com/artifact/org.scala-lang.modules/scala-xml
+libraryDependencies += "org.scala-lang.modules" %% "scala-xml" % "2.0.0-M3"
+
---
+++ src/main/scala/XMLParsing.scala
@@ -0,0 +1,74 @@
+import scala.xml.XML
+//import scala.xml
+
+object XMLParsing extends App {
+  //so these XML tags we just made up for this particular exercise, they can match HTML but not necessarily
+  val a = <article>An article about something</article> //so we did not even need to write quotes around our text
+  println(a) //so a is an xml Elem
+  val p = <p>Just a paragraph</p>
+  println(p)
+
+  val pod = <pod>Three <peas>Some <justEmpty/>Peaas</peas> in the </pod> //example of Nesting
+  println(pod)
+  val pod2 = <pod peas="3" strings="true"/> //example of XML tag with attributes self closing
+  println(pod2)
+
+  val myName = "Valdis"
+  val greetings = <greeting>Hello there {myName}</greeting> // so i can interpolate Scala code into XML element
+  //interpolation can be dangerous if we have no control of what goes inside the { }
+  //not a good practice for user inputs, an evil user could input some evil code
+  println(greetings)
+  println(greetings.text)
+  println(pod.text) //so we get ALL of the text of parent and its childrend and grandchildren and so on
+
+  val relativePath = "./src/resources/lnb-4.xml"
+  //loads XML file and parses
+  val xml = XML.loadFile(relativePath) //so val xml is points to the root element of this particular document
+  println(xml)
+
+  val digitalObject = xml \ "digitalObject"
+  println(digitalObject)
+
+
+
+//  val digitalChildren = digitalObject.head.child //of course we'd need to check length here
+//  println("*"*40)
+//  digitalChildren.foreach(println)
+
+  val xmlChildren = xml.child
+  xmlChildren.foreach(el => println(el.label))
+
+  val title = xml \ "title"
+  println(title)
+  println(s"Title of this Object is ${title.text} ")
+
+  val subjects = xml \ "subject"
+  subjects.foreach(println)
+  val uris = subjects.map(subject => subject.attribute("uri"))  //of course here we know that uri attribute exists
+
+  subjects.foreach(subject => println(subject.attribute("uri")))
+
+  val copyRight = xml \ "copyright"
+  println(copyRight.length)
+  println(copyRight.head) //we could possible have multiple matches with copyright tag
+  println(copyRight.head.attribute("code"))
+  println(copyRight.head.attribute("code").getOrElse("Did not find it"))
+
+  val fileNames = xml \ "files" \ "file"
+  for (file <- fileNames) {
+    println(file.attribute("name").getOrElse("File Name not found"))
+  }
+
+  val fileNameStrings = for (file <- fileNames)  yield file.attribute("name").getOrElse("File Name not found")
+  fileNameStrings.foreach(println)
+
+  //you have the whole xml stored in val xml
+  //TODO get text contents of subType
+  //remember that most matches are in some sort of sequence because in XML you could have multiple matches
+  //TODO get size attribute for each file (this is in bytes)
+
+  //We will do this after 21:00
+  //TODO hardest challenge get size in KBytes from each files <fileMetadata><field name="Size">228 KB</field> .... </fileMetadata>
+
+
+}
