5f0c1ff27a304e575568c435e7d5e52b69872cc8
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-03-15 21:39:07 +0200

    Linear Increase before fix

--- src/main/scala/LargestIncrease.scala
+++ src/main/scala/LargestIncrease.scala
@@ -15,7 +15,7 @@ object LargestIncrease extends App {
 
 
 
-  val testSeq = Seq(5,9,3,8,6,7,1,4)
+  val testSeq = Seq(5,9,3,7,8,6,7,1,4)
 
   //we can always start with a brute force solution
   //this simply means we try every value combination in this case
@@ -29,7 +29,7 @@ object LargestIncrease extends App {
         if (vAfter - v > increase) {
 
           increase = vAfter - v
-          println(s"New winner! ${increase} from $vAfter - $v")
+//          println(s"New winner! ${increase} from $vAfter - $v")
         }
       }
     }
@@ -37,9 +37,46 @@ object LargestIncrease extends App {
   }
 
   println(findBiggestIncrease(testSeq))
-  println(findBiggestIncrease(integers))
+
 
   //TODO solve biggest increase in so called Linear time - meaning single loop
+  def findBiggestIncreaseLin(ints: Seq[Int]):Int = {
+    var min = ints(0)
+//    var max = ints(0)
+    var relativeWinner = 0
+    var absoluteWinner = 0
+    for ((n, i) <- ints.tail.zipWithIndex) {
+      val delta = n - min //this should work because ints indexing is one less than our values
+      if (delta > relativeWinner) relativeWinner = delta
+      if (relativeWinner > absoluteWinner) absoluteWinner = relativeWinner
+      if (n < min) {
+        min = n //so we have a new minimum we need to start keeping track of new records for diff
+        relativeWinner = 0 //this is important because we will need to calculate diff again
+      }
+    }
+
+    absoluteWinner
+  }
+  println("Testing Linear Algorithm")
+  println(findBiggestIncreaseLin(testSeq))
+  println(findBiggestIncreaseLin((1 to 10)))
+  println(findBiggestIncreaseLin((10 to 1 by -1 )))
+  println(findBiggestIncreaseLin(Seq(4,7,2,10,1,6,10,3,6)))
+
+  var t0 = System.nanoTime()
+  val increase1 = findBiggestIncreaseLin(integers)
+  var t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0) + "ns")
+
+  t0 = System.nanoTime()
+  val increase2 = findBiggestIncrease(integers)
+  t1 = System.nanoTime()
+  println("Elapsed time: " + (t1 - t0) + "ns")
+
+  println(s"Biggest Increase is $increase1 == $increase2")
+
+
+
   //it could actually be a couple loops but the key being that they are not nested
   //so 3 loops in a row (not within each other) would still be Linear
   //because number 3 does not change as more data is added
@@ -54,9 +91,33 @@ object LargestIncrease extends App {
   //you could just write the code first in comments then try to implement it
   //you could Google for it too, there are multiple solutions possible
 
-  def findBiggestIncreaseLinear(ints: Seq[Int]):Int = {
-    0 //FIXME
-  }
+//  def findBiggestIncreaseLinear(ints: Seq[Int]):Int = {
+//    var sumDiff = 0
+//    var maxSumDiff = ints.min - ints.max - 1
+//    var maxDiffIndex = -1
+//    var minSumDiff = - maxSumDiff
+//    var minDiffIndex = -1
+//
+//    var previous = ints(0)
+//    for (i <- 1 to ints.length-1) {
+//      val diff = ints(i) - previous
+//      sumDiff = sumDiff + diff
+//
+//      if (sumDiff > maxSumDiff) {
+//        maxSumDiff = sumDiff
+//        maxDiffIndex = i
+//      }
+//
+//      if (sumDiff < minSumDiff) {
+//        minSumDiff = sumDiff
+//        minDiffIndex = i
+//      }
+//      previous = ints(i)
+//    }
+//    val increase = ints(maxDiffIndex) - ints(minDiffIndex)
+//    increase
+//  }
 
+//  println(findBiggestIncreaseLinear(testSeq))
 
 }
