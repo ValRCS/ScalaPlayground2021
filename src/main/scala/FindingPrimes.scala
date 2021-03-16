cf16e553a415a4313423cef2312ec7215b464b5a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-03-16 19:54:45 +0200

    Guessing Game exercise

---
+++ src/main/scala/FindingPrimes.scala
@@ -0,0 +1,36 @@
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
+import scala.io.StdIn.readLine
+// import package
+import scala.util.control.Breaks._
+
+
+object FindingPrimes extends App {
+//  val howMany = 10
+  val howMany = readLine("How many primes to find?").toInt
+  val hardNumberLimit = 1_000_000
+//  val primes = for (n <- 1 to howMany) yield {
+//    while
+//  }
+  val primes = ArrayBuffer(2)
+
+  var curNumber = 3
+  while (primes.length < howMany && curNumber < hardNumberLimit) {
+    breakable {
+      val lastCheck = Math.ceil(Math.sqrt(curNumber)).toInt
+      val divisors = Seq(2) ++ (3 to lastCheck by 2)
+      for (n <- divisors) { //we know the number divides by itself and also by 1
+        if (curNumber % n == 0) {
+          println(s"curNumber $curNumber divides evenly with $n that's not a prime")
+          break
+        }//so if the current candidate divides evenly by any number under itself (not counting 1) it is not a prime
+      }
+      println(s"$curNumber is a prime!")
+      primes += curNumber //we add it to our prime collection
+    }
+    curNumber += 1
+  }
+
+  println(primes.mkString(","))
+//  primes.foreach(println)
+}
---
+++ src/main/scala/GuessingGame.scala
@@ -0,0 +1,14 @@
+object GuessingGame extends App {
+//TODO Write a guessing game where the user has to guess a secret number.
+  //you will need to create a secret number using random
+  // After every guess the program tells the user whether their number was too large or too small.
+  // At the end the number of tries needed should be printed.
+  // It counts only as one try if they input the same number multiple times consecutively.
+
+
+  //remember we can use readLine
+  //most likely a while loop
+  //do not need a breakable unless you want to try it out
+
+  //there was a game i think 20 questions where you have to guess something
+}
