a8f4f2e20618c2fb9ea3bdfa0d8ea18be76a1e3c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-04-01 19:17:38 +0300

    US 500 CSV parsing

--- src/main/scala/ParsingPersonCSV.scala
+++ src/main/scala/ParsingPersonCSV.scala
@@ -8,4 +8,8 @@ object ParsingPersonCSV extends App {
   //  val cols = lines.map(line => line.split(","))
   //we split each line into tokens by , separator an then trim whitespace from each token
   val cols = lines.map(line => line.split(",").map(_.trim))
+
+  //TODO well get minimum number or entries and maximum number of entries in those cols
+  //WE want to test if parsing went correctly meaning min and max should be equal == 12
+  // a little sanity check before we proceed any further
 }
