c260b1cb6d01df41fc6a32ea20c800a7d9ab240b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-04-06 21:16:08 +0300

    Laura's solution with clamp

--- src/main/scala/Nim.scala
+++ src/main/scala/Nim.scala
@@ -1,5 +1,8 @@
 package com.github.valrcs
 
+import com.github.valrcs.Utilities.clamp //fully qualified import with full name
+//import com.github.valrcs._ //imports everything in my package
+
 import scala.io.StdIn.readLine
 
 object Nim extends App {
@@ -20,7 +23,11 @@ object Nim extends App {
   var computerLevel = 0 //we could have save a variable and used computer level as a selector meaning computerLevel 0 would be human :)
   val isPlayerBComputer = readLine("Do you want to play against computer (Y/N)?").contains("Y") //we could have added a more complex if
   if (isPlayerBComputer) computerLevel = readLine("How strong a computer you want (1-3)? ").toInt //TODO Clamp function with (minMatches and maxMatches)
-  //TODO add clamp to our utilites file
+  computerLevel = clamp(computerLevel, minMatches, maxMatches)
+  //for one off calls this would also work with fullname
+//  computerLevel =  com.github.valrcs.Utilities.clamp(computerLevel, minMatches, maxMatches)
+//  computerLevel =  Utilities.clamp(computerLevel, minMatches, maxMatches) //works because of wildcard import with _
+  //careful with wildcard imports you might sometimes get a collision
 
 
   def computerMove(matches: Int, computerLevel:Int): Int = {
@@ -32,7 +39,12 @@ object Nim extends App {
   }
 
   def smartComputer(matches:Int): Int = {
-    3 //FIXME make me smart!
+    val take = matches match {
+      case 4 | 8 | 12 | 16 | 20 => 3
+      case 3 | 7 | 11 | 15 | 19 => 2
+      case _ => 1
+    }
+    take //TODO make it more universal not only for 21
   }
 
   //we need loop for this
