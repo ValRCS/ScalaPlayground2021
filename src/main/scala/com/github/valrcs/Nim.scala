56fd93fce64ab5251e1210989a8c510d3e31cc71
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-04-06 21:59:40 +0300

    Added GameState class to create objects to store game state

--- src/main/scala/com/github/valrcs/Nim.scala
+++ src/main/scala/com/github/valrcs/Nim.scala
@@ -4,25 +4,35 @@ import com.github.valrcs.Utilities.clamp
 
 import scala.io.StdIn.readLine
 
+//so everything related to game state lives in separate Object
+//template for our game truths
+class GameState(startingMatchCount: Int = 21, minMatches:Int = 1, maxMatches:Int = 3, startComputerLevel:Int = 0) {
+  var matches = startingMatchCount
+  val min = minMatches
+  val max = maxMatches
+  var isPlayerATurn = true //at the beginning of game Player A starts
+  var isPlayerBComputer = false
+  var computerLevel = startComputerLevel
+  //this is bare minimum for our game
+//we could have save a variable and used computer level as a selector meaning computerLevel 0 would be human :)
+}
+
+
+
 object Nim extends App {
   //https://en.wikipedia.org/wiki/Nim#The_21_game
   println("Let's play a game of Nim!")
   //TODO implement 2 player version with 21 matches and up to 3 matches taken in one turn
-
+  val state = new GameState() //creating a new game state object with default parameters
   //we will want to have some state for our game
   //in our case our game state will be simple just an integer holding count of our matches
-  var matches = 21 // our game state
-  var isPlayerATurn = true //at the beginning of game Player A starts
-  //  var isPlayerBComputer = false
-  //this is bare minimum for our game
-  val minMatches = 1
-  val maxMatches = 3
+
   val r = new scala.util.Random
 
-  var computerLevel = 0 //we could have save a variable and used computer level as a selector meaning computerLevel 0 would be human :)
+
   val isPlayerBComputer = readLine("Do you want to play against computer (Y/N)?").contains("Y") //we could have added a more complex if
-  if (isPlayerBComputer) computerLevel = readLine("How strong a computer you want (1-3)? ").toInt //TODO Clamp function with (minMatches and maxMatches)
-  computerLevel = clamp(computerLevel, minMatches, maxMatches)
+  if (isPlayerBComputer) state.computerLevel = readLine("How strong a computer you want (1-3)? ").toInt //TODO Clamp function with (minMatches and maxMatches)
+  state.computerLevel = clamp(state.computerLevel, state.min, state.max)
   //for one off calls this would also work with fullname
   //  computerLevel =  com.github.valrcs.Utilities.clamp(computerLevel, minMatches, maxMatches)
   //  computerLevel =  Utilities.clamp(computerLevel, minMatches, maxMatches) //works because of wildcard import with _
@@ -32,7 +42,7 @@ object Nim extends App {
   def computerMove(matches: Int, computerLevel: Int): Int = {
     computerLevel match {
       case 1 => 1
-      case 2 => r.between(minMatches, maxMatches + 1) //between last number is exclusive
+      case 2 => r.between(state.min, state.max + 1) //between last number is exclusive
       case 3 => smartComputer(matches)
     }
   }
@@ -51,31 +61,31 @@ object Nim extends App {
       //we always want a default case
       //here default is same as matches % 4 == 1
       //this is the case when computer is losing
-      case _ => r.between(minMatches, maxMatches + 1) //idea being that if computer is in bad shape it should do random choice
+      case _ => r.between(state.min, state.max + 1) //idea being that if computer is in bad shape it should do random choice
     }
     take
   }
 
   //we need loop for this
-  while (matches > 0) {
-    println(s"We have $matches left, it is ${getPlayerTurn(isPlayerATurn)} turn")
+  while (state.matches > 0) {
+    println(s"We have ${state.matches} left, it is ${getPlayerTurn(state.isPlayerATurn)} turn")
 
     var matchesTaken = 0
     //computer check
-    if (!isPlayerATurn && isPlayerBComputer) matchesTaken = computerMove(matches, computerLevel)
+    if (!state.isPlayerATurn && isPlayerBComputer) matchesTaken = computerMove(state.matches, state.computerLevel)
 
-    while (matchesTaken < minMatches || matchesTaken > maxMatches) {
-      println(s"Please choose between $minMatches and $maxMatches matches")
-      matchesTaken = readLine(s"How many matches do you want to take ${getPlayerTurn(isPlayerATurn)}?").toInt
+    while (matchesTaken < state.min || matchesTaken > state.max) {
+      println(s"Please choose between ${state.min} and ${state.max} matches")
+      matchesTaken = readLine(s"How many matches do you want to take ${getPlayerTurn(state.isPlayerATurn)}?").toInt
     }
 
-    matches -= matchesTaken
-    isPlayerATurn = !isPlayerATurn //old trick on toggling boolean value
+    state.matches -= matchesTaken
+    state.isPlayerATurn = !state.isPlayerATurn //old trick on toggling boolean value
     //showing Game State
 
   }
 
-  println(s"Congratulations you won ${getPlayerTurn(isPlayerATurn)}!")
+  println(s"Congratulations you won ${getPlayerTurn(state.isPlayerATurn)}!")
 
   def getPlayerTurn(isPlayerATurn: Boolean): String = if (isPlayerATurn) "Player A" else if (isPlayerBComputer) "Computer" else "Player B"
 
